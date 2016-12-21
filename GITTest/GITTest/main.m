45b3467464e3c6aa3d9603be354a14dc42986171
yishujun <ysj@boyoi.com>
2016-12-21 22:55:03 +0800

    初始化项目

---
+++ GITTest/GITTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GITTest
+//
+//  Created by 易术军 on 2016/12/21.
+//  Copyright © 2016年 Boyoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
